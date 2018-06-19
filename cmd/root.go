c621959d9123d1e20c892cd2275562853b91069b
Alberto Bregliano <alberto.bregliano@pm.me>
2018-06-19 17:36:00 +0200

    voice on or off

--- cmd/root.go
+++ cmd/root.go
@@ -32,9 +32,6 @@ import (
 
 var cfgFile string
 
-//Voicecall gestisce l'invio o meno delle notifiche telefoniche
-var Voicecall bool
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "sarumann",
@@ -66,7 +63,6 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sarumann.yaml)")
-	rootCmd.PersistentFlags().BoolVarP(&Voicecall, "voicecall", "v", true, "Attivazione chiamate vocali")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
